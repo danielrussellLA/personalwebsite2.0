d4e6e46fc78371452f39518ce4412c431257b7ff
danielrussellLA <danielrussell16@gmail.com>
2016-08-14 22:11:50 -0700

    improved grid layout

--- src/components/app.js
+++ src/components/app.js
@@ -4,13 +4,13 @@ import NavBar from './navbar';
 export default class App extends Component {
   render() {
     return (
-      <div className="ui grid">
-          <div className="five wide column">
-              <NavBar/>
-          </div>
-          <div className="eleven wide column">
-              {this.props.children}
-          </div>
+      <div className="ui padded grid">
+        <div className="five wide column">
+          <NavBar/>
+        </div>
+        <div className="eleven wide column">
+          {this.props.children}
+        </div>
       </div>
     );
   }
